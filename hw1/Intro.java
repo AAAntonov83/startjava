e6181c33b84473687d154a29c8a28b5f92b80b7e
Andrei Antonov <aaakuzbass@gmail.com>
2023-01-10 20:59:17 +0400

    Revert "Revert "Test""
    This reverts commit e7f404e814d898887b8c158a4656480c384f2907.

--- hw1/Intro.java
+++ hw1/Intro.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Intro{
     public static void main(String[] args) {
-        System.out.println("Enter your name.");
+        System.out.println("Enter your name please.");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
         System.out.println("Welcome to the JDK, " + name + "!!");
