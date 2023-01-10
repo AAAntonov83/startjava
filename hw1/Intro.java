b2c63b9ce3dc9b3aa7ff1fbb70e7affa5d35eca5
Andrei Antonov <aaakuzbass@gmail.com>
2023-01-10 21:00:03 +0400

    Revert "new commit"
    This reverts commit 0a6feb0d743d7dd541d8848f2ab2a24feedcb2a2.

--- hw1/Intro.java
+++ hw1/Intro.java
@@ -5,6 +5,6 @@ public class Intro{
         System.out.println("Enter your name please.");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
-        System.out.println("Welcome to the JDK, " + name + "!!");
+        System.out.println("Welcome to the JDK, " + name + "!");
     }
 }
