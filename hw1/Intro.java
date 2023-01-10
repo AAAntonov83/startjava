8317ae146e24b943d8dfd6ee6784a6671b645232
Andrei Antonov <aaakuzbass@gmail.com>
2023-01-10 19:53:54 +0400

    Second commit

--- hw1/Intro.java
+++ hw1/Intro.java
@@ -1,5 +1,8 @@
 public class Intro{
     public static void main(String[] args) {
-        System.out.println("Welcome to the JDK!");
+        System.out.println("Enter your name.");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Welcome to the JDK, " + name + "!");
     }
 }
