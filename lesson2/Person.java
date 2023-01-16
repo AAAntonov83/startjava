b646a9f699e7b020c3efb823c47e5bb0f5f703d6
Andrei Antonov <aaakuzbass@gmail.com>
2023-01-16 16:13:33 +0400

    lesson2 start && Person.java add

---
+++ lesson2/Person.java
@@ -0,0 +1,17 @@
+public class Person {
+    private String gender = "Male";
+    private String name = "John Rambo";
+    private int age = 35;
+
+    public void walk() {
+    }
+
+    public void run() {
+    }
+
+    public void say() {
+    }
+
+    public void learn() {
+    }
+}
