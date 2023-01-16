c34e1bfa8aaa61af7f64dec56eb0a2c2889d64dc
Andrei Antonov <aaakuzbass@gmail.com>
2023-01-16 17:13:12 +0400

    Person.java fix

--- lesson2/Person.java
+++ lesson2/Person.java
@@ -5,17 +5,22 @@ public class Person {
     int age = 35;
 
     void walk() {
+        System.out.println("Иду по важным делам");
     }
 
     void run() {
+        System.out.println("Побежал от важных дел");
     }
 
     void shave() {
+        System.out.println("Бреюсь ножом");
     }
 
     void talk() {
+        System.out.println("М-м-м-м-м.");
     }
 
     void learnJava() {
+        System.out.println("Какая еще Жава?!");
     }
 }
