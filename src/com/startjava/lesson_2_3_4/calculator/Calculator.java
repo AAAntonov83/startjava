891fa40963e5a41a91c42581189ccc108b334b18
Andrei Antonov <aaakuzbass@gmail.com>
2023-01-26 21:48:37 +0400

    Calculator fix

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -2,15 +2,28 @@ package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
 
-    public static double calculate(int number1, int number2, String operation) throws Exception {
-        return switch (operation) {
-            case "+" -> Math.addExact(number1, number2);
-            case "-" -> Math.subtractExact(number1, number2);
-            case "*" -> Math.multiplyExact(number1, number2);
-            case "/" -> (double) number1 / number2;
-            case "^" -> Math.pow(number1, number2);
-            case "%" -> Math.IEEEremainder(number1, number2);
+    public static void calculate(String expression) throws Exception {
+
+        String[] expressionParts = expression.split(" ");
+        int factor1 = Integer.parseInt(expressionParts[0]);
+        int factor2 = Integer.parseInt(expressionParts[2]);
+        String operation = expressionParts[1];
+
+        if (factor1 < 0 || factor2 < 0) {
+            throw new Exception("Математические операции только с положительными числами.");
+        }
+
+        double result = switch (operation) {
+            case "+" -> Math.addExact(factor1, factor2);
+            case "-" -> Math.subtractExact(factor1, factor2);
+            case "*" -> Math.multiplyExact(factor1, factor2);
+            case "/" -> (double) factor1 / factor2;
+            case "^" -> Math.pow(factor1, factor2);
+            case "%" -> Math.IEEEremainder(factor1, factor2);
             default ->  throw new Exception("Введена неверная математическая операция");
         };
+
+        System.out.printf("%s %s %s = %" + (result % 1 > 0 ? ".3" : ".0") + "f%n",
+                factor1, operation, factor2, result);
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -11,21 +11,10 @@ public class CalculatorTest {
         while (!answer.equals("no")) {
             if (answer.equals("yes")) {
                 System.out.print("Введите математическое выражение: ");
-                String expString = scanner.nextLine();
-                String[] expression = expString.split(" ");
+                String expression = scanner.nextLine();
 
                 try {
-                    int factor1 = Integer.parseInt(expression[0]);
-                    int factor2 = Integer.parseInt(expression[2]);
-
-                    if (factor1 < 0 || factor2 < 0) {
-                        throw new Exception("Математические операции только с положительными числами.");
-                    }
-
-                    double result = Calculator.calculate(Integer.parseInt(expression[0]),
-                            Integer.parseInt(expression[2]), expression[1]);
-                    System.out.printf("%s %s %s = %" + (result % 1 > 0 ? ".3" : ".0") + "f%n",
-                            expression[0], expression[1], expression[2], result);
+                    Calculator.calculate(expression);
                 } catch (IndexOutOfBoundsException e){
                     System.out.println("Ввод некорректен.");
                 } catch (NumberFormatException e){
