4270c8a8a7242d5ad6e25fe87b785716d35231a4
Andrei Antonov <aaakuzbass@gmail.com>
2023-01-26 21:40:27 +0400

    Calculator fix

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -2,21 +2,15 @@ package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
 
-    public double calculate(int number1, int number2, String operation) {
-        switch (operation) {
-            case "+":
-                return Math.addExact(number1, number2 );
-            case "-":
-                return Math.subtractExact(number1, number2) ;
-            case "*":
-                return Math.multiplyExact(number1, number2);
-            case "/":
-                return (double) number1 / number2;
-            case "^":
-                return Math.pow(number1, number2);
-            case "%":
-                return Math.IEEEremainder(number1, number2);
-        }
-        return 0;
+    public static double calculate(int number1, int number2, String operation) throws Exception {
+        return switch (operation) {
+            case "+" -> Math.addExact(number1, number2);
+            case "-" -> Math.subtractExact(number1, number2);
+            case "*" -> Math.multiplyExact(number1, number2);
+            case "/" -> (double) number1 / number2;
+            case "^" -> Math.pow(number1, number2);
+            case "%" -> Math.IEEEremainder(number1, number2);
+            default ->  throw new Exception("Введена неверная математическая операция");
+        };
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -13,12 +13,26 @@ public class CalculatorTest {
                 System.out.print("Введите математическое выражение: ");
                 String expString = scanner.nextLine();
                 String[] expression = expString.split(" ");
-                Calculator calculator = new Calculator();
 
-                double result = calculator.calculate(Integer.parseInt(expression[0]),
-                        Integer.parseInt(expression[2]), expression[1]);
-                System.out.printf("%s %s %s = %" + (result % 1 > 0 ? ".3" : ".0") + "f%n",
-                        expression[0], expression[1], expression[2], result);
+                try {
+                    int factor1 = Integer.parseInt(expression[0]);
+                    int factor2 = Integer.parseInt(expression[2]);
+
+                    if (factor1 < 0 || factor2 < 0) {
+                        throw new Exception("Математические операции только с положительными числами.");
+                    }
+
+                    double result = Calculator.calculate(Integer.parseInt(expression[0]),
+                            Integer.parseInt(expression[2]), expression[1]);
+                    System.out.printf("%s %s %s = %" + (result % 1 > 0 ? ".3" : ".0") + "f%n",
+                            expression[0], expression[1], expression[2], result);
+                } catch (IndexOutOfBoundsException e){
+                    System.out.println("Ввод некорректен.");
+                } catch (NumberFormatException e){
+                    System.out.println("Математические операции вычисляются только с целыми числами.");
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
             }
             System.out.println("Хотите продолжить вычисления? [yes/no]");
             answer = scanner.nextLine();
